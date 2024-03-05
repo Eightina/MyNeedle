6b80f8daee87fddd3a7f4876fb369e4e8d17153e
Orion <orion.que@outlook.com>
2024-03-05 17:09:43 +0800

    kaiming uniform supports multidimention

--- python/needle/init/init_initializers.py
+++ python/needle/init/init_initializers.py
@@ -18,11 +18,19 @@ def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
 
 def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
+    
     gain = 2 ** (1 / 2)
     bound = gain * ((3 / fan_in) ** (1 / 2))
     kwargs['high'] = bound
     kwargs['low'] = -bound
-    return rand(fan_in, fan_out, **kwargs)
+    
+    if shape is None:
+        new_in, new_out = fan_in, fan_out
+        return rand(new_in, new_out, **kwargs)
+    else:
+        assert len(shape) == 4, "For a conv kernel only 4 dim is allowed"
+        assert shape[0] == shape[1], "multiple values of K"
+        return rand(*shape, **kwargs)
 
 def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
